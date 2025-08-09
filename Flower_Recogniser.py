cb6672854eb70ae1b8e6b107979f62392f8e5eaf
Burak Ozturk <120913735+Burak-ai@users.noreply.github.com>
2025-08-09 23:21:48 +0300

    Update Flower_Recogniser.py

--- Flower_Recogniser.py
+++ Flower_Recogniser.py
@@ -5,34 +5,38 @@ from tensorflow.keras.preprocessing.image import ImageDataGenerator
 from tensorflow.keras import layers, models
 from sklearn.model_selection import train_test_split
 
-data_dir = "Flowers"
-img_height = 128
-img_width = 128
-batch_size = 32
 
-datagen = ImageDataGenerator(
-    rescale = 1./255,
-    rotation_range = 20,
-    width_shift_range = 0.2,
-    height_shift_range = 0.2,
-    shear_range = 0.2,
-    horizontal_flip = True,
-    fill_mode = "nearest"
-    )
-
-train_generator = datagen.flow_from_directory(
-    data_dir,
-    target_size = (img_height, img_width),
-    batch_size = batch_size,
-    class_mode="Categorical"
+train_datagen = ImageDataGenerator(
+    rescale=1./255,
+    rotation_range=20,
+    width_shift_range=0.2,
+    height_shift_range=0.2,
+    shear_range=0.2,
+    horizontal_flip=True,
+    fill_mode='nearest',
 )
 
-val_datagen = ImageDataGenerator(rescale=1./255)
 
-valid_generator =  val_datagen.flow_from_directory(
-    data_dir,
-    target_size=(img_height, img_width),
-    batch_size=batch_size,
-    class_mode='categorical'
+val_datagen = ImageDataGenerator(
+    rescale=1./255,
+    validation_split=0.2
+)
+
+# 80% of data
+train_generator = train_datagen.flow_from_directory(
+    'Flowers', 
+    target_size=(128, 128),
+    batch_size=32,
+    class_mode='categorical',
+    subset='training'  
+)
+
+# 20% of data
+validation_generator = val_datagen.flow_from_directory(
+    'Flowers',
+    target_size=(128, 128),
+    batch_size=32,
+    class_mode='categorical',
+    subset='validation' 
 )
 
